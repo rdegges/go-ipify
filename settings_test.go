247e100b2835b3b47d8ce0e3919db20245c55633
Randall Degges <r@rdegges.com>
2015-05-25 17:11:41 -0700

    Adding tests for the version in user agent.

--- settings_test.go
+++ settings_test.go
@@ -10,4 +10,8 @@ func TestUserAgent(t *testing.T) {
 	if !strings.Contains(strings.ToLower(USER_AGENT), runtime.GOOS) {
 		t.Error("USER_AGENT doesn't contain the OS type:", USER_AGENT)
 	}
+
+	if !strings.Contains(USER_AGENT, VERSION) {
+		t.Error("USER_AGENT doesn't contain the library version:", USER_AGENT)
+	}
 }
