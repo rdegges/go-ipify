2536ec12485c391b419a82289bd29299f83fab94
Randall Degges <r@rdegges.com>
2015-05-25 17:07:29 -0700

    Implementing an ipify.GetIp() function.

---
+++ ipify.go
@@ -0,0 +1,50 @@
+package ipify
+
+import (
+	"errors"
+	"github.com/jpillora/backoff"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// GetIp queries the ipify service (http://www.ipify.org) to retrieve this
+// machine's public IP address.
+func GetIp() (error, string) {
+	b := &backoff.Backoff{
+		Jitter: true,
+	}
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", API_URI, nil)
+	if err != nil {
+		return errors.New("Received an invalid status code from ipify: 500. The service might be experiencing issues."), ""
+	}
+
+	req.Header.Add("User-Agent", USER_AGENT)
+
+	for tries := 0; tries < MAX_TRIES; tries++ {
+		resp, err := client.Do(req)
+		if err != nil {
+			d := b.Duration()
+			time.Sleep(d)
+			continue
+		}
+
+		defer resp.Body.Close()
+
+		ip, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return errors.New("Received an invalid status code from ipify: 500. The service might be experiencing issues."), ""
+		}
+
+		if resp.StatusCode != 200 {
+			return errors.New("Received an invalid status code from ipify: " + strconv.Itoa(resp.StatusCode) + ". The service might be experiencing issues."), ""
+		}
+
+		return nil, string(ip)
+	}
+
+	return errors.New("The request failed because it wasn't able to reach the ipify service. This is most likely due to a networking error of some sort."), ""
+}
